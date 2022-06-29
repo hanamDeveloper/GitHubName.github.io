c9ae1b4640f6bbe1365dd87fb4caf1601abf10a9
hanamDeveloper <hanamDeveloper@github.com>
2022-06-29 10:02:27 +0900

    레거시 코드 제거

--- candleStick/index.js
+++ candleStick/index.js
@@ -91,21 +91,6 @@ function splitData(rawData) {
     values: values,
   };
 }
-function calculateMA(dayCount) {
-  var result = [];
-  for (var i = 0, len = data0.values.length; i < len; i++) {
-    if (i < dayCount) {
-      result.push("-");
-      continue;
-    }
-    var sum = 0;
-    for (var j = 0; j < dayCount; j++) {
-      sum += +data0.values[i - j][1];
-    }
-    result.push(sum / dayCount);
-  }
-  return result;
-}
 option = {
   tooltip: {
     trigger: "axis",
